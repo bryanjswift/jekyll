67865b5709b2f9403b67db02fe594a76dd70e440
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-05 20:09:10 -0500

    Make includes get parsed if they are markdown or textile
    Layouts don't work

--- lib/jekyll/tags/include.rb
+++ lib/jekyll/tags/include.rb
@@ -1,29 +1,57 @@
 module Jekyll
 
   class IncludeTag < Liquid::Tag
+    include Convertible
+
+    attr_accessor :site
+    attr_accessor :name, :ext, :basename
+    attr_accessor :data, :content, :output
+
     def initialize(tag_name, file, tokens)
       super
       @file = file.strip
+    end
+
+    def process(context)
+      pieces = @file.split(".")
+      alt = context
+      pieces.each do |piece|
+        if (alt && alt.has_key?(piece))
+          alt = alt[piece]
+        else
+          alt = false
+        end
+      end
+      self.site = context.registers[:site]
+      self.name = alt || @file
+      self.ext = File.extname(self.name)
+      dir = File.join(self.site.source, "_includes")
+      if (File.exist? File.join(dir,self.name))
+        self.read_yaml(dir, self.name)
+      else
+        self.data = {}
+      end
+      self.name
     end
 
     def render(context)
       if @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
         return "Include file '#{@file}' contains invalid characters or sequences"
       end
-
-      Dir.chdir(File.join(context.registers[:site].source, '_includes')) do
+      
+      self.process(context)
+      Dir.chdir(File.join(self.site.source, '_includes')) do
         choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-        if choices.include?(@file)
-          source = File.read(@file)
-          partial = Liquid::Template.parse(source)
-          context.stack do
-            partial.render(context)
-          end
+        if choices.include?(self.name)
+          self.content = File.read(self.name)
+          do_layout(context,self.site.layouts)
+          self.output
         else
           "Included file '#{@file}' not found in _includes directory"
         end
       end
     end
+
   end
 
 end
